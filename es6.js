5b95bf60b9c46138e6b86b20fd226313e02a86d5
aar-atiq <aar.msdeveloper01@gmail.com>
2021-02-02 12:00:45 +0600

    class inheritance

--- es6.js
+++ es6.js
@@ -1,9 +1,18 @@
-class Child {
+class Father {
+    constructor(){
+        this.fatherName = "All-Jobbar";
+    }
+}
+
+class Child extends Father{
     constructor(name1, ages1, position1){
+        super();
         this.name = name1;
         this.ages = ages1;
         this.position = position1;
     }
 }
-const showUp = new Child("bokkor", 25, "enginner");
+const showUp = new Child("bokkor", 25, "engineer");
 console.log(showUp);
+const altShowUp = new Child("jhokkor", 30, "engineer");
+console.log(altShowUp);

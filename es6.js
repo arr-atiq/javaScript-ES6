32109dcbe23006bbce5b88453bb9d1b566b64efb
aar-atiq <aar.msdeveloper01@gmail.com>
2021-02-02 11:52:04 +0600

    knowing ES6

---
+++ es6.js
@@ -0,0 +1,9 @@
+class Child {
+    constructor(name1, ages1, position1){
+        this.name = name1;
+        this.ages = ages1;
+        this.position = position1;
+    }
+}
+const showUp = new Child("bokkor", 25, "enginner");
+console.log(showUp);
